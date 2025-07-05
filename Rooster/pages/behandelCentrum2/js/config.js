fef41d93fac167891b7ee32bb07e8c5ecea9482a
Wes van Bussel <wvbussel91@gmail.com>
2025-07-05 18:51:19 +0200

    reset

--- Rooster/pages/behandelCentrum2/js/config.js
+++
@@ -1,7 +0,0 @@
-const appConfiguratie = {
-    instellingen: {
-        siteUrl: "https://som.org.om.local/sites/MulderT/CustomPW/Verlof/"
-    },
-    CompensatieUren: { lijstTitel: "CompensatieUren" },
-    Verlof: { lijstTitel: "Verlof" },
-};
